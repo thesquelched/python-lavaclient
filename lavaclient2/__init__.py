f94c26804dd3904bc180921011602f2a38c44448
Scott Kruger <scott@chojin.org>
2015-02-05 14:36:22 -0600

    Update version

--- lavaclient2/__init__.py
+++ lavaclient2/__init__.py
@@ -10,7 +10,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-__version_info__ = (0, 1, 'alpha1')
+__version_info__ = (0, 1, '1', 'alpha')
 __version__ = '.'.join(map(str, __version_info__))
 
 
